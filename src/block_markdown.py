7671301a28d4a10f7ea3a67e8f7bc0c694b4f295
david.voronov <davidvoronof@gmail.com>
2024-07-14 18:53:56 -0700

    Added rest of the project

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,3 +1,11 @@
+import re
+
+block_type_paragraph = "paragraph"
+block_type_heading = "heading"
+block_type_code = "code"
+block_type_quote = "quote"
+block_type_unordered_list = "unordered_list"
+block_type_ordered_list = "ordered_list"
 
 def markdown_to_blocks(markdown):
     blocks = markdown.split("\n\n")
@@ -7,4 +15,29 @@ def markdown_to_blocks(markdown):
         if block != "":
             filtered_blocks.append(block)
     return filtered_blocks
+
+# We assume we are getting perfect blocks that have been previously split. We are just "disallowing" bad blocks and categorizing them as paragraph by default.
+def block_to_block_type(block):
+    if (re.match(r"#{1,6}.*?", block) != None):
+        return block_type_heading
+    elif (re.match(r"```.*?```", block) != None):
+        return block_type_code
+    elif (re.match(r">.*?", block) != None):
+        return block_type_quote
+    elif (len(re.findall(r"((\*|-)\s.*)", block)) == len(block.split("\n"))):
+        return block_type_unordered_list
+    
+    # Something a little more complex for the ordered list needs to happen. Regex (at least to my knowledge) won't be able to support incrementing so im going to need a little for loop
+    block_lines = block.split("\n")
+    i = 1
+    for line in block_lines:
+        if (line[0:3] != f"{i}. "):
+            return block_type_paragraph
+        i += 1
+    return block_type_ordered_list
+    
+
+
+
+
 
--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -17,3 +17,25 @@ class TestMarkdownToBlocks(unittest.TestCase):
         ])
 
 
+class TestBlockToBlockType(unittest.TestCase):
+    def test_paragraph(self):
+        block = "this is just a paragraph block"
+        self.assertEqual(block_to_block_type(block), "paragraph")
+    def test_heading(self):
+        block = "### this is a triple heading block"
+        self.assertEqual(block_to_block_type(block), "heading")
+    def test_code(self):
+        block = "```this\nis\na\ncode block```"
+        self.assertEqual(block_to_block_type(block), "code")
+    def test_code(self):
+        block = "> this is a quote block"
+        self.assertEqual(block_to_block_type(block), "quote")
+    def test_unordered_list(self):
+        block = "* this is an unordered list\n- this is the second line\n* this is the third line\n- this is the fourth line"
+        self.assertEqual(block_to_block_type(block), "unordered_list")
+    def test_bad_unordered_list(self):
+        block = "* this is a deceptive\nunordered list because its actually not"
+        self.assertEqual(block_to_block_type(block), "paragraph")
+    def test_ordered_list(self):
+        block = "1. This is 1\n2. This is 2\n3. This is 3\n4. This is 4"
+        self.assertEqual(block_to_block_type(block), "ordered_list")
